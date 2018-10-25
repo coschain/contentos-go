e87de302fc04cd39c4208e00d066ca2f5d73f99b
yykingking <yykingking@gmail.com>
2018-10-25 20:36:04 +0800

    add const value file

---
+++ common/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+const (
+	COS_CHAIN_NAME		= "contentos"
+	COS_INIT_SUPPLY		= 10000000000
+	COS_CONSENSUS_NAME	= "iBFT"
+	COS_INIT_MINER		= "initminer"
+
+	COIN_SYMBOL			= "COS"
+	VEST_SYMBOL			= "VEST"
+
+	BLOCK_INTERNAL		= 3000			// 3000 ms for one block produce
+)
+
