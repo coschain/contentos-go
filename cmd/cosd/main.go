f3b7618183ed165fdf88678890e26bcfa69d3ee1
AprocySanae <axdiaoqi220@gmail.com>
2018-10-25 20:07:35 +0800

    add cobra and viper

--- cmd/cosd/main.go
+++ cmd/cosd/main.go
@@ -1,9 +1,54 @@
 package main
 
+import (
+	"fmt"
+	log "github.com/inconshreveable/log15"
+	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 const (
 	clientIdentifier = "cosd"
 )
 
-func main() {
+// NO OTHER CONFIG HERE EXCEPT NODE CONFIG
+func cmdRunNode(cmd *cobra.Command, args []string) {
+	// _ is cfg as below process has't used
+	node, _ := makeConfig()
+	if err := node.Start(); err != nil {
+		fmt.Println("Fatal: ", err)
+		os.Exit(1)
+	}
 
+	go func() {
+		sigc := make(chan os.Signal, 1)
+		signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+		defer signal.Stop(sigc)
+		<-sigc
+		log.Info("Got interrupt, shutting down...")
+		go node.Stop()
+	}()
+}
+
+// cosd is the main entry point into the system if no special subcommand is pointed
+// It creates a default node based on the command line arguments and runs it
+// in blocking mode, waiting for it to be shut down.
+var rootCmd = &cobra.Command{
+	Use:   "cosd",
+	Short: "Cosd is a fast blockchain for content",
+	Run:   cmdRunNode,
+}
+
+func addCommands() {
+
+}
+
+func main() {
+	addCommands()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- node/default.go
+++ node/default.go
@@ -1,6 +1,43 @@
 package node
 
+import (
+	"github.com/coschain/contentos-go/p2p"
+	"github.com/coschain/contentos-go/p2p/nat"
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
 const (
 	DefaultHTTPHost = "localhost"
 	DefaultHTTPPort = 8123
 )
+
+var DefaultNodeConfig = Config{
+	DataDir:  DefaultDataDir(),
+	HTTPHost: DefaultHTTPHost,
+	HTTPPort: DefaultHTTPPort,
+	P2P: p2p.Config{
+		ListenAddr: ":30303",
+		MaxPeers:   25,
+		NAT:        nat.Any(),
+	},
+}
+
+func DefaultDataDir() string {
+	home := homeDir()
+	if home != "" {
+		return filepath.Join(home, ".coschain")
+	}
+	return ""
+}
+
+func homeDir() string {
+	if home := os.Getenv("HOME"); home != "" {
+		return home
+	}
+	if usr, err := user.Current(); err == nil {
+		return usr.HomeDir
+	}
+	return ""
+}
