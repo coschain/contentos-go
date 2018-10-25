9a8bf1d0c2d3cf178fd841c631ed35b6ca6ad88d
hanyunlong <chrisarthur@163.com>
2018-10-25 22:23:33 +0800

    1.remove some unessary 2.mod a bug

--- cmd/cosd/main.go
+++ cmd/cosd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	log "github.com/inconshreveable/log15"
+	//log "github.com/inconshreveable/log15"
 	"github.com/spf13/cobra"
 	"os"
-	"os/signal"
-	"syscall"
+	//"os/signal"
+	//"syscall"
 )
 
 const (
@@ -22,14 +22,18 @@ func cmdRunNode(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
-	go func() {
-		sigc := make(chan os.Signal, 1)
-		signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
-		defer signal.Stop(sigc)
-		<-sigc
-		log.Info("Got interrupt, shutting down...")
-		go node.Stop()
-	}()
+	node.Wait()
+
+	/*
+		go func() {
+			sigc := make(chan os.Signal, 1)
+			signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+			defer signal.Stop(sigc)
+			<-sigc
+			log.Info("Got interrupt, shutting down...")
+			go node.Stop()
+		}()
+	*/
 }
 
 // cosd is the main entry point into the system if no special subcommand is pointed
@@ -52,77 +56,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-/*
-type gethConfig struct {
-	Node node.Config
-}
-
-func main() {
-	node := makeFullNode()
-
-	fmt.Println("1")
-
-	startNode(node)
-
-	fmt.Println("2")
-
-	node.Wait()
-}
-
-func makeFullNode() *node.Node {
-	stack, _ := makeConfigNode()
-
-	return stack
-}
-
-func makeConfigNode() (*node.Node, gethConfig) {
-	cfg := gethConfig{
-		Node: defaultNodeConfig(),
-	}
-
-	stack, err := node.New(&cfg.Node)
-	if err != nil {
-		fmt.Println("Failed to create the protocol stack: %v", err)
-	}
-
-	return stack, cfg
-}
-
-func defaultNodeConfig() node.Config {
-	cfg := node.DefaultConfig
-	cfg.Name = clientIdentifier
-	//cfg.Version = params.VersionWithCommit(gitCommit)
-	return cfg
-}
-
-func startNode(stack *node.Node) {
-
-	// Start up the node itself
-	StartNode(stack)
-
-}
-
-func StartNode(stack *node.Node) {
-	if err := stack.Start(); err != nil {
-		fmt.Println("Error starting protocol stack: %v", err)
-	}
-	go func() {
-		sigc := make(chan os.Signal, 1)
-		signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
-		defer signal.Stop(sigc)
-		<-sigc
-		fmt.Println("Got interrupt, shutting down...")
-		go stack.Stop()
-		for i := 10; i > 0; i-- {
-			<-sigc
-			if i > 1 {
-				fmt.Println("Already shutting down, interrupt more to panic. ", " times: ", i-1)
-			}
-		}
-		//debug.Exit() // ensure trace and CPU profile data is flushed.
-		//debug.LoudPanic("boom")
-	}()
->>>>>>> add p2p entrypoint, can listen port
-}
-*/
