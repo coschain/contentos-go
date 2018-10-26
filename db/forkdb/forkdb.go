9018508547eb2b5fc20d024d5c89e0ae1858d522
zhaoguojie2010 <zhaoguojie2010@163.com>
2018-10-26 11:59:00 +0800

    forkdb add: Remove, Head, Pop

--- db/forkdb/forkdb.go
+++ db/forkdb/forkdb.go
@@ -1,6 +1,8 @@
 package forkdb
 
 import (
+	"fmt"
+
 	"github.com/coschain/contentos-go/common"
 )
 
@@ -31,17 +33,40 @@ func NewDB() *DB {
 
 // Remove removes a block
 func (db *DB) Remove(id common.BlockID) {
-
+	num := id.BlockNum()
+	if num > db.head.BlockNum()+1 || num < db.start {
+		return
+	}
+	delete(db.branches, id)
+	delete(db.detached, id)
+	idx := num - db.start + db.offset
+	for i := range db.list[idx] {
+		if db.list[idx][i] == id {
+			l := len(db.list[idx])
+			db.list[idx][i], db.list[idx][l-1] = db.list[idx][l-1], db.list[idx][i]
+			db.list[idx] = db.list[idx][:l-1]
+		}
+	}
 }
 
 // FetchBlock fetches a block corresponding to id
 func (db *DB) FetchBlock(id common.BlockID) (common.SignedBlock, error) {
-	return nil, nil
+	b, ok := db.branches[id]
+	if ok {
+		return b, nil
+	}
+	return nil, fmt.Errorf("No block has id of %v", id)
 }
 
 // FetchBlockByNum fetches a block corresponding to the block num
-func (db *DB) FetchBlockByNum(num uint64) ([]common.SignedBlock, error) {
-	return nil, nil
+func (db *DB) FetchBlockByNum(num uint64) []common.SignedBlock {
+	list := db.list[num-db.start+db.offset]
+	ret := make([]common.SignedBlock, len(list))
+	for i := range list {
+		b, _ := db.branches[list[i]]
+		ret[i] = b
+	}
+	return ret
 }
 
 // PushBlock adds a block. If any of the forkchain has more than
@@ -49,7 +74,7 @@ func (db *DB) FetchBlockByNum(num uint64) ([]common.SignedBlock, error) {
 func (db *DB) PushBlock(b common.SignedBlock) common.SignedBlock {
 	id := b.Id()
 	num := id.BlockNum()
-	if len(db.list) == 0 {
+	if len(db.branches) == 0 {
 		db.head = id
 		db.start = num
 		db.list[0] = append(db.list[0], db.head)
@@ -119,12 +144,25 @@ func (db *DB) purge() {
 // Head returns the head block of the longest chain, returns nil
 // if the db is empty
 func (db *DB) Head() common.SignedBlock {
-	return nil
+	if len(db.branches) == 0 {
+		return nil
+	}
+
+	return db.branches[db.head]
 }
 
-// Pop pops the head block of the longest chain
-func (db *DB) Pop() {
-
+// Pop pops the head block
+// NOTE: The only senario Pop should be called is when a fork switch
+// occurs, hence the main branch and the fork branch has a common ancestor
+// that should NEVER be poped, which also means the main branch cannot be
+// poped empty
+func (db *DB) Pop() common.SignedBlock {
+	ret := db.branches[db.head]
+	db.head = ret.Previous()
+	if _, ok := db.branches[db.head]; !ok {
+		panic("The main branch was poped empty")
+	}
+	return ret
 }
 
 // FetchNewBranch finds the nearest ancestor of b1 and b2, then returns
