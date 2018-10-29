505a9a699687b1e1b37d87ade925168197ee5de1
AprocySanae <axdiaoqi220@gmail.com>
2018-10-29 20:55:55 +0800

    adjust files and logic

--- cmd/cosd/main.go
+++ cmd/cosd/main.go
@@ -3,20 +3,53 @@ package main
 import (
 	"fmt"
 	"github.com/coschain/contentos-go/cmd/cosd/commands"
+	"github.com/coschain/contentos-go/node"
+	"github.com/ethereum/go-ethereum/cmd/utils"
 	log "github.com/inconshreveable/log15"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 )
 
-// NO OTHER CONFIGS HERE EXCEPT NODE CONFIG
-func cmdRunNode(cmd *cobra.Command, args []string) {
-	// _ is cfg as below process has't used
-	node, _ := makeConfig()
-	if err := node.Start(); err != nil {
+func makeNode() (*node.Node, node.Config) {
+	cfg := node.DefaultNodeConfig
+	cfg.Name = commands.ClientIdentifier
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	confdir := filepath.Join(cfg.DataDir, cfg.Name)
+	viper.AddConfigPath(confdir)
+	err := viper.ReadInConfig()
+	if err == nil {
+		viper.Unmarshal(&cfg)
+	} else {
+		fmt.Printf("fatal: not be initialized (do `init` first)\n")
+		os.Exit(1)
+	}
+	if cfg.DataDir != "" {
+		dir, err := filepath.Abs(cfg.DataDir)
+		if err != nil {
+			utils.Fatalf("DataDir in cfg cannot be converted to absolute path")
+		}
+		cfg.DataDir = dir
+	}
+	app, err := node.New(&cfg)
+	if err != nil {
 		fmt.Println("Fatal: ", err)
 		os.Exit(1)
+	}
+	return app, cfg
+
+}
+
+// NO OTHER CONFIGS HERE EXCEPT NODE CONFIG
+func cmdStartNode(cmd *cobra.Command, args []string) {
+	// _ is cfg as below process has't used
+	app, _ := makeNode()
+	if err := app.Start(); err != nil {
+		utils.Fatalf("start node failed, err: %v\n", err)
 	}
 
 	go func() {
@@ -25,9 +58,9 @@ func cmdRunNode(cmd *cobra.Command, args []string) {
 		defer signal.Stop(sigc)
 		<-sigc
 		log.Info("Got interrupt, shutting down...")
-		go node.Stop()
+		go app.Stop()
 	}()
-	node.Wait()
+	app.Wait()
 }
 
 // cosd is the main entry point into the system if no special subcommand is pointed
@@ -35,8 +68,8 @@ func cmdRunNode(cmd *cobra.Command, args []string) {
 // in blocking mode, waiting for it to be shut down.
 var rootCmd = &cobra.Command{
 	Use:   "cosd",
-	Short: "Cosd is a fast blockchain for content",
-	Run:   cmdRunNode,
+	Short: "Cosd is a fast blockchain designed for content",
+	Run:   cmdStartNode,
 }
 
 func addCommands() {
--- common/utils.go
+++ common/utils.go
@@ -1,16 +1,43 @@
 package common
 
-
+import (
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+)
 
 func Int2Bytes(n uint32) []byte {
 	var b []byte
 	var i int
-	for i=0;i<4;i++ {b=append(b,0)}
-	i=4
-	for (n>0 && i>0) {
-		i--;
-		b[i]=byte(n&0xff)
-		n/=256
+	for i = 0; i < 4; i++ {
+		b = append(b, 0)
+	}
+	i = 4
+	for n > 0 && i > 0 {
+		i--
+		b[i] = byte(n & 0xff)
+		n /= 256
 	}
 	return b
+}
+
+// Fatalf formats a message to standard error and exits the program.
+// The message is also printed to standard output if standard error
+// is redirected to a different file.
+func Fatalf(format string, args ...interface{}) {
+	w := io.MultiWriter(os.Stdout, os.Stderr)
+	if runtime.GOOS == "windows" {
+		// The SameFile check below doesn't work on Windows.
+		// stdout is unlikely to get redirected though, so just print there.
+		w = os.Stdout
+	} else {
+		outf, _ := os.Stdout.Stat()
+		errf, _ := os.Stderr.Stat()
+		if outf != nil && errf != nil && os.SameFile(outf, errf) {
+			w = os.Stderr
+		}
+	}
+	fmt.Fprintf(w, "Fatal: "+format+"\n", args...)
+	os.Exit(1)
 }
