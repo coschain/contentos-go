9a1bb827d3b450b9fd98b0bb0933b062c44bb994
huoxin <nwpudonkey@gmail.com>
2018-11-09 17:13:47 +0800

    create blockSummaryobject and transactionObject

--- app/controller.go
+++ app/controller.go
@@ -3,6 +3,7 @@ package app
 import (
 	"bytes"
 	"github.com/asaskevich/EventBus"
+	"github.com/coschain/contentos-go/app/table"
 	"github.com/coschain/contentos-go/common/constants"
 	"github.com/coschain/contentos-go/common/eventloop"
 	"github.com/coschain/contentos-go/common/prototype"
@@ -24,31 +25,30 @@ type Controller struct {
 	// lock for db write
 	// pending_trx_list
 	// DB Manager
-	ctx *node.ServiceContext
+	ctx    *node.ServiceContext
 	evLoop *eventloop.EventLoop
 
-	db      *AppDBLayer
+	db      storage.IDatabaseService
 	noticer EventBus.Bus
 	skip    skipFlag
 
-	_pending_tx   []*prototype.TransactionWrapper
-	_isProducing  bool
-	_currentTrxId *prototype.Sha256
+	_pending_tx        []*prototype.TransactionWrapper
+	_isProducing       bool
+	_currentTrxId      *prototype.Sha256
 	_current_op_in_trx uint16
 
-	revertDb *storage.RevertibleDatabase
+	revertDb      *storage.RevertibleDatabase
 	transactionDb *storage.TransactionalDatabase
-
 }
 
 // service constructor
 func NewController(ctx *node.ServiceContext) (*Controller, error) {
-	return &Controller{ ctx: ctx}, nil
+	return &Controller{ctx: ctx}, nil
 }
 
 func (c *Controller) Start(node *node.Node) error {
-	c.evLoop 	= node.MainLoop
-	c.noticer 	= node.EvBus
+	c.evLoop = node.MainLoop
+	c.noticer = node.EvBus
 	return nil
 }
 
@@ -140,7 +140,7 @@ func (c *Controller) processBlock() {
 }
 
 func (c *Controller) _applyTransaction(trxWrp *prototype.TransactionWrapper) {
-	defer func(){
+	defer func() {
 		if err := recover(); err != nil {
 			trxWrp.Invoice.Status = 500
 			panic("_applyTransaction failed")
@@ -159,23 +159,34 @@ func (c *Controller) _applyTransaction(trxWrp *prototype.TransactionWrapper) {
 
 	trx.Validate()
 
-	// @ trx duplicate check
+	// trx duplicate check
+	transactionObjWrap := table.NewSoTransactionObjectWrap(c.db, c._currentTrxId)
+	if transactionObjWrap.CheckExist() {
+		panic("Duplicate transaction check failed")
+	}
 
 	if c.skip&skip_transaction_signatures == 0 {
-		tmpChainId := prototype.ChainId{Value:0}
-		trx.VerifyAuthority(tmpChainId,2)
+		tmpChainId := prototype.ChainId{Value: 0}
+		trx.VerifyAuthority(tmpChainId, 2)
 		// @ check_admin
 	}
 
 	// @ TaPos and expired check
+	//if headBlockNum() > 0
 
-	// @ insert trx into DB unique table
+	// insert trx into DB unique table
+	obj := &table.SoTransactionObject{}
+	obj.TrxId = c._currentTrxId
+	obj.Expiration = &prototype.TimePointSec{UtcSeconds: 100}
+	if !transactionObjWrap.CreateTransactionObject(obj) {
+		panic("create transactionObject failed")
+	}
 
 	c.NotifyTrxPreExecute(trx)
 
 	// process operation
 	c._current_op_in_trx = 0
-	for _,op := range trx.Trx.Operations {
+	for _, op := range trx.Trx.Operations {
 		c.applyOperation(op)
 		c._current_op_in_trx++
 	}
@@ -183,15 +194,15 @@ func (c *Controller) _applyTransaction(trxWrp *prototype.TransactionWrapper) {
 	c._currentTrxId = &prototype.Sha256{}
 }
 
-func (c *Controller) applyOperation(op *prototype.Operation){
-	n := &prototype.OperationNotification{Op:op}
+func (c *Controller) applyOperation(op *prototype.Operation) {
+	n := &prototype.OperationNotification{Op: op}
 	c.NotifyOpPreExecute(n)
 	eva := getEvaluator(op)
 	eva.Apply(op)
 	c.NotifyOpPostExecute(n)
 }
 
-func getEvaluator(op *prototype.Operation) BaseEvaluator{
+func getEvaluator(op *prototype.Operation) BaseEvaluator {
 	switch op.Op.(type) {
 	case *prototype.Operation_Op1:
 		return BaseEvaluator(&AccountCreateEvaluator{})
@@ -204,7 +215,7 @@ func getEvaluator(op *prototype.Operation) BaseEvaluator{
 
 func (c *Controller) applyBlock(blk *prototype.SignedBlock) {
 	oldFlag := c.skip
-	defer func(){
+	defer func() {
 		c.skip = oldFlag
 	}()
 
@@ -219,5 +230,14 @@ func (c *Controller) _applyBlock(blk *prototype.SignedBlock) {
 	root := blk.CalculateMerkleRoot()
 	if !bytes.Equal(root.Data[:], blk.SignedHeader.Header.TransactionMerkleRoot.Hash) {
 		panic("Merkle check failed")
+	}
+}
+
+func (c *Controller) InitGenesis() {
+	for i := int32(0); i < 0x10000; i++ {
+		wrap := table.NewSoBlockSummaryObjectWrap(c.db, &i)
+		obj := &table.SoBlockSummaryObject{}
+		obj.Id = i
+		wrap.CreateBlockSummaryObject(obj)
 	}
 }
