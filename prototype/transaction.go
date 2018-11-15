0d181748903da5a03368a5b420f05ce72590a194
yykingking <yykingking@gmail.com>
2018-11-15 14:44:10 +0800

    add operations

--- prototype/transaction.go
+++ prototype/transaction.go
@@ -15,6 +15,38 @@ func (m *Transaction) AddOperation(op interface{}) {
 		ptr := op.(*TransferOperation)
 		res.Op = &Operation_Op2{Op2: ptr}
 		break
+	case *BpRegisterOperation:
+		ptr := op.(*BpRegisterOperation)
+		res.Op = &Operation_Op3{Op3: ptr}
+		break
+	case *BpUnregisterOperation:
+		ptr := op.(*BpUnregisterOperation)
+		res.Op = &Operation_Op4{Op4: ptr}
+		break
+	case *BpVoteOperation:
+		ptr := op.(*BpVoteOperation)
+		res.Op = &Operation_Op5{Op5: ptr}
+		break
+	case *PostOperation:
+		ptr := op.(*PostOperation)
+		res.Op = &Operation_Op6{Op6: ptr}
+		break
+	case *ReplayOperation:
+		ptr := op.(*ReplayOperation)
+		res.Op = &Operation_Op7{Op7: ptr}
+		break
+	case *FollowOperation:
+		ptr := op.(*FollowOperation)
+		res.Op = &Operation_Op8{Op8: ptr}
+		break
+	case *VoteOperation:
+		ptr := op.(*VoteOperation)
+		res.Op = &Operation_Op9{Op9: ptr}
+		break
+	case *TransferToVestingOperation:
+		ptr := op.(*TransferToVestingOperation)
+		res.Op = &Operation_Op10{Op10: ptr}
+		break
 	default:
 		panic("error op type")
 	}
