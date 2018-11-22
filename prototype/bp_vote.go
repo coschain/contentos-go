d1b0a6db5b38b6b4264d170f35adbe9abddeaaa3
daishuo <daishuo@gmail.com>
2018-11-22 18:59:48 +0800

    review kope.OpeEncoder implements & minor fix

--- prototype/bp_vote.go
+++ prototype/bp_vote.go
@@ -3,5 +3,5 @@ package prototype
 import "github.com/coschain/contentos-go/common/encoding/kope"
 
 func (m *BpVoterId) OpeEncode() ([]byte, error) {
-	return kope.Encode(m.Voter.Value + "|" + m.Witness.Value)
+	return kope.Encode(m.Voter.Value, m.Witness.Value)
 }
