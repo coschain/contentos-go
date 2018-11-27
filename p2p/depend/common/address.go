7615f9e818955f00ed288d7423fb369d8ec54986
hanyunlong <chrisarthur@163.com>
2018-11-27 21:17:46 +0800

    mod code

--- p2p/depend/common/address.go
+++ p2p/depend/common/address.go
@@ -1,13 +1,8 @@
 package common
 
 import (
-	"crypto/sha256"
 	"errors"
-	"fmt"
 	"io"
-	"math/big"
-
-	"github.com/itchyny/base58-go"
 )
 
 const ADDR_LEN = 20
@@ -15,11 +10,6 @@ const ADDR_LEN = 20
 type Address [ADDR_LEN]byte
 
 var ADDRESS_EMPTY = Address{}
-
-// ToHexString returns  hex string representation of Address
-func (self *Address) ToHexString() string {
-	return fmt.Sprintf("%x", ToArrayReverse(self[:]))
-}
 
 // Serialize serialize Address into io.Writer
 func (self *Address) Serialize(w io.Writer) error {
@@ -34,16 +24,4 @@ func (self *Address) Deserialize(r io.Reader) error {
 		return errors.New("deserialize Address error")
 	}
 	return nil
-}
-
-// ToBase58 returns base58 encoded address string
-func (f *Address) ToBase58() string {
-	data := append([]byte{23}, f[:]...)
-	temp := sha256.Sum256(data)
-	temps := sha256.Sum256(temp[:])
-	data = append(data, temps[0:4]...)
-
-	bi := new(big.Int).SetBytes(data).String()
-	encoded, _ := base58.BitcoinEncoding.Encode([]byte(bi))
-	return string(encoded)
 }
