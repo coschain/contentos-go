a15f966abf4579b33a8dd85f1f0291b398d93fa9
hanyunlong <chrisarthur@163.com>
2018-11-27 21:07:39 +0800

    remove unecessary code

--- p2p/depend/common/address.go
+++ p2p/depend/common/address.go
@@ -8,7 +8,6 @@ import (
 	"math/big"
 
 	"github.com/itchyny/base58-go"
-	"golang.org/x/crypto/ripemd160"
 )
 
 const ADDR_LEN = 20
@@ -47,68 +46,4 @@ func (f *Address) ToBase58() string {
 	bi := new(big.Int).SetBytes(data).String()
 	encoded, _ := base58.BitcoinEncoding.Encode([]byte(bi))
 	return string(encoded)
-}
-
-// AddressParseFromBytes returns parsed Address
-func AddressParseFromBytes(f []byte) (Address, error) {
-	if len(f) != ADDR_LEN {
-		return ADDRESS_EMPTY, errors.New("[Common]: AddressParseFromBytes err, len != 20")
-	}
-
-	var addr Address
-	copy(addr[:], f)
-	return addr, nil
-}
-
-// AddressParseFromHexString returns parsed Address
-func AddressFromHexString(s string) (Address, error) {
-	hx, err := HexToBytes(s)
-	if err != nil {
-		return ADDRESS_EMPTY, err
-	}
-	return AddressParseFromBytes(ToArrayReverse(hx))
-}
-
-// AddressFromBase58 returns Address from encoded base58 string
-func AddressFromBase58(encoded string) (Address, error) {
-	if encoded == "" {
-		return ADDRESS_EMPTY, errors.New("invalid address")
-	}
-	decoded, err := base58.BitcoinEncoding.Decode([]byte(encoded))
-	if err != nil {
-		return ADDRESS_EMPTY, err
-	}
-
-	x, ok := new(big.Int).SetString(string(decoded), 10)
-	if !ok {
-		return ADDRESS_EMPTY, errors.New("invalid address")
-	}
-
-	buf := x.Bytes()
-	if len(buf) != 1+ADDR_LEN+4 || buf[0] != byte(23) {
-		return ADDRESS_EMPTY, errors.New("wrong encoded address")
-	}
-
-	ph, err := AddressParseFromBytes(buf[1:21])
-	if err != nil {
-		return ADDRESS_EMPTY, err
-	}
-
-	addr := ph.ToBase58()
-
-	if addr != encoded {
-		return ADDRESS_EMPTY, errors.New("[AddressFromBase58]: decode encoded verify failed.")
-	}
-
-	return ph, nil
-}
-
-func AddressFromVmCode(code []byte) Address {
-	var addr Address
-	temp := sha256.Sum256(code)
-	md := ripemd160.New()
-	md.Write(temp[:])
-	md.Sum(addr[:0])
-
-	return addr
 }
--- p2p/depend/common/common.go
+++ p2p/depend/common/common.go
@@ -1,27 +1,8 @@
 package common
 
 import (
-	"encoding/hex"
-	"math/rand"
 	"os"
 )
-
-// GetNonce returns random nonce
-func GetNonce() uint64 {
-	// Fixme replace with the real random number generator
-	nonce := uint64(rand.Uint32())<<32 + uint64(rand.Uint32())
-	return nonce
-}
-
-// ToHexString convert []byte to hex string
-func ToHexString(data []byte) string {
-	return hex.EncodeToString(data)
-}
-
-// HexToBytes convert hex string to []byte
-func HexToBytes(value string) ([]byte, error) {
-	return hex.DecodeString(value)
-}
 
 func ToArrayReverse(arr []byte) []byte {
 	l := len(arr)
