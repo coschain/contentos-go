c9b74201b0f1a98f9c27fff5c3972ded25d90b90
daishuo <daishuo@gmail.com>
2018-12-18 12:29:34 +0800

    vme: type signature based conversion from json

--- common/encoding/vme/json_test.go
+++ common/encoding/vme/json_test.go
@@ -1,19 +1,14 @@
 package vme
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestEncodeJsonArray(t *testing.T) {
-	_, err := EncodeJsonArray("[3, 3.4, false, \"hello\", [2,4,5], [\"abc\",\"de\"]]", []reflect.Type{
-		reflect.TypeOf(int32(0)),
-		reflect.TypeOf(uint64(0)),
-		reflect.TypeOf(false),
-		reflect.TypeOf(""),
-		reflect.TypeOf([]int16{}),
-		reflect.TypeOf([][]byte{}),
-	})
+	jsonStr := `[ 3, 3.4, false, "hello", [2, 4, 5], ["abc", "de"], [[[[[[true]]]]]] ]`
+	sig := "d;d;Z;s;[Q;[s;[[[[[[Z"
+
+	_, err := EncodeJsonArrayWithTypeSig(jsonStr, sig)
 	if err != nil {
 		t.Fatal(err)
 	}
