ef6347b44e32f5fcc70b48ac771f926c8f452792
yykingking <yykingking@gmail.com>
2019-03-05 16:22:23 +0800

    fix precision error

--- prototype/coin.go
+++ prototype/coin.go
@@ -34,11 +34,13 @@ func (m *Coin) Sub(o *Coin) error {
 }
 
 func (m *Coin) ToString() string {
-	var result float64
+	var result = m.Value
 
-	result = float64(m.Value * 1.0) / float64(constants.COSTokenDecimals * 1.0)
-	return fmt.Sprintf("%.6f %s",
-		result,
+	var high = result / constants.COSTokenDecimals
+	var low = result % constants.COSTokenDecimals
+
+	return fmt.Sprintf("%d.%06d %s",
+		high,low,
 		constants.CoinSymbol)
 }
 
