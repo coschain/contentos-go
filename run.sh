971398343af1ff5a4c5a51bf7be1d3df996eb8d4
hanyunlong <chrisarthur@163.com>
2019-03-07 18:45:03 +0800

    docker self can delete or reserve db

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 work_path=$(dirname $(readlink -f $0))
+choice=`cat ${work_path}/set.txt`
+case $choice in
+    delete)
+        cd /root/.coschain
+        rm -rf *
+        ;;
+    reserve)
+        ;;
+    *)
+        echo "Unknown choice, it should be delete or reserve, your choice is $choice"
+        exit 1
+        ;;
+esac
+
+
 #${work_path}/bin/cosd init
 exec ${work_path}/bin/cosd start
