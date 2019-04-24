e323c20511084c3405bb625739d9c1296930e1a9
aprocysanae <axdiaoqi220@gmail.com>
2019-04-24 17:50:20 +0800

    add creator

--- cmd/cosd/commands/db.go
+++ cmd/cosd/commands/db.go
@@ -65,19 +65,22 @@ func initDb(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 	createTrxInfo := `create table trxinfo
-	(
-		trx_id varchar(64) not null,
-		block_height int unsigned not null,
-		block_time int unsigned not null,
-		invoice json null,
-		operations json null,
-		block_id varchar(64) null,
-		INDEX trxinfo_block_height_index (block_height),
-		INDEX trxinfo_block_time_index (block_time),
-		INDEX trxinfo_block_id (block_id),
-		constraint trxinfo_trx_id_uindex
-	unique (trx_id)
-	)`
+(
+	trx_id varchar(64) not null,
+	block_height int unsigned not null,
+	block_time int unsigned not null,
+	invoice json null,
+	operations json null,
+	block_id varchar(64) not null,
+	creator varchar(64) not null,
+	INDEX trxinfo_block_height_index (block_height),
+	INDEX trxinfo_block_time_index (block_time),
+	INDEX trxinfo_block_id (block_id),
+	INDEX trxinfo_block_creator (creator),
+	constraint trxinfo_trx_id_uindex
+		unique (trx_id)
+);
+	`
 	createLibInfo := `create table libinfo
 (
 	lib int unsigned not null,
