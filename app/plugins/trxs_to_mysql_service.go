05bfbec6d9c64e673aa57989bc47e7036a816bc5
aprocysanae <axdiaoqi220@gmail.com>
2019-05-07 13:43:33 +0800

    stat run too fast

--- app/plugins/trxs_to_mysql_service.go
+++ app/plugins/trxs_to_mysql_service.go
@@ -174,7 +174,7 @@ func (t *TrxMysqlService) pollLIB() error {
 	var waitingSyncLib []uint64
 	var count = 0
 	for lastLib < lib {
-		if count > 100 {
+		if count > 1000 {
 			break
 		}
 		waitingSyncLib = append(waitingSyncLib, lastLib)
@@ -225,12 +225,12 @@ func (t *TrxMysqlService) handleLibNotification(lib uint64) {
 		//operation := trx.SigTrx.GetTrx().GetOperations()[0]
 		creator := FindCreator(trx.SigTrx.GetTrx().GetOperations()[0])
 		t1 := time.Now()
-		_, _ = t.outDb.Exec("INSERT INTO trxinfo (trx_id, block_height, block_id, block_time, invoice, operations, creator)  value (?, ?, ?, ?, ?, ?, ?)", trxId, blockHeight, blockId, blockTime, invoice, operationsJson, creator)
+		_, _ = t.outDb.Exec("INSERT IGNORE INTO trxinfo (trx_id, block_height, block_id, block_time, invoice, operations, creator)  value (?, ?, ?, ?, ?, ?, ?)", trxId, blockHeight, blockId, blockTime, invoice, operationsJson, creator)
 		t.log.Debugf("[trx db] insert trxinfo spent: %v", time.Now().Sub(t1))
 		t2 := time.Now()
 		for _, operation := range trx.SigTrx.GetTrx().GetOperations() {
 			if IsCreateAccountOp(operation) {
-				_, _ = t.outDb.Exec("INSERT INTO createaccountinfo (trx_id, create_time, creator, pubkey, account) values (?, ?, ?, ?, ?)", trxId, blockTime, creator, operation.GetOp1().Owner.ToWIF(), operation.GetOp1().NewAccountName.Value)
+				_, _ = t.outDb.Exec("INSERT IGNORE INTO createaccountinfo (trx_id, create_time, creator, pubkey, account) values (?, ?, ?, ?, ?)", trxId, blockTime, creator, operation.GetOp1().Owner.ToWIF(), operation.GetOp1().NewAccountName.Value)
 				break
 			}
 		}
@@ -238,7 +238,7 @@ func (t *TrxMysqlService) handleLibNotification(lib uint64) {
 		t3 := time.Now()
 		for _, operation := range trx.SigTrx.GetTrx().GetOperations() {
 			if IsTransferOp(operation) {
-				_, _ = t.outDb.Exec("INSERT INTO transferinfo (trx_id, create_time, sender, receiver, amount, memo) values (?, ?, ?, ?, ?, ?)", trxId, blockTime, creator, operation.GetOp2().To.Value, operation.GetOp2().Amount.Value, operation.GetOp2().Memo)
+				_, _ = t.outDb.Exec("INSERT IGNORE INTO transferinfo (trx_id, create_time, sender, receiver, amount, memo) values (?, ?, ?, ?, ?, ?)", trxId, blockTime, creator, operation.GetOp2().To.Value, operation.GetOp2().Amount.Value, operation.GetOp2().Memo)
 				break
 			}
 		}
