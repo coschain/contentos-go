f1b40c60a1b08164c527e642321e613c127ac6c2
aprocysanae <axdiaoqi220@gmail.com>
2019-05-10 18:26:24 +0800

    using db to store all block_log

--- app/plugins/dbtables/statelog.sql
+++ app/plugins/dbtables/statelog.sql
@@ -4,18 +4,25 @@ create table stateloglibinfo
   last_check_time int unsigned not null
 );
 
+# create table statelog
+# (
+#   id bigint unsigned primary key auto_increment,
+#   block_id varchar(64),
+#   block_height int unsigned,
+#   trx_id varchar(64),
+#   action smallint,
+#   property varchar(64),
+#   state json,
+#   INDEX statelog_block_id_index (block_id),
+#   INDEX statelog_block_height_index (block_height),
+#   INDEX statelog_trx_id_index (trx_id)
+# );
+
 create table statelog
 (
-  id bigint unsigned primary key auto_increment,
-  block_id varchar(64),
   block_height int unsigned,
-  trx_id varchar(64),
-  action smallint,
-  property varchar(64),
-  state json,
-  INDEX statelog_block_id_index (block_id),
-  INDEX statelog_block_height_index (block_height),
-  INDEX statelog_trx_id_index (trx_id)
+  block_log json,
+  UNIQUE KEY statelog_block_height (block_height)
 );
 
 create table stateaccount
