68dcb9577a53e095e98400a1be00886786f85662
wangchao <wangchao4055@gmail.com>
2019-05-13 14:24:56 +0800

    add rpc api:GetContractListByTime

--- app/table/so_contract.pb.go
+++ app/table/so_contract.pb.go
@@ -34,16 +34,17 @@ func (m *SoContract) Reset()         { *m = SoContract{} }
 func (m *SoContract) String() string { return proto.CompactTextString(m) }
 func (*SoContract) ProtoMessage()    {}
 func (*SoContract) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{0}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{0}
 }
+
 func (m *SoContract) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoContract.Unmarshal(m, b)
 }
 func (m *SoContract) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoContract.Marshal(b, m, deterministic)
 }
-func (dst *SoContract) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoContract.Merge(dst, src)
+func (m *SoContract) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoContract.Merge(m, src)
 }
 func (m *SoContract) XXX_Size() int {
 	return xxx_messageInfo_SoContract.Size(m)
@@ -100,16 +101,17 @@ func (m *SoMemContractById) Reset()         { *m = SoMemContractById{} }
 func (m *SoMemContractById) String() string { return proto.CompactTextString(m) }
 func (*SoMemContractById) ProtoMessage()    {}
 func (*SoMemContractById) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{1}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{1}
 }
+
 func (m *SoMemContractById) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoMemContractById.Unmarshal(m, b)
 }
 func (m *SoMemContractById) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoMemContractById.Marshal(b, m, deterministic)
 }
-func (dst *SoMemContractById) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoMemContractById.Merge(dst, src)
+func (m *SoMemContractById) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoMemContractById.Merge(m, src)
 }
 func (m *SoMemContractById) XXX_Size() int {
 	return xxx_messageInfo_SoMemContractById.Size(m)
@@ -138,16 +140,17 @@ func (m *SoMemContractByCreatedTime) Reset()         { *m = SoMemContractByCreat
 func (m *SoMemContractByCreatedTime) String() string { return proto.CompactTextString(m) }
 func (*SoMemContractByCreatedTime) ProtoMessage()    {}
 func (*SoMemContractByCreatedTime) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{2}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{2}
 }
+
 func (m *SoMemContractByCreatedTime) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoMemContractByCreatedTime.Unmarshal(m, b)
 }
 func (m *SoMemContractByCreatedTime) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoMemContractByCreatedTime.Marshal(b, m, deterministic)
 }
-func (dst *SoMemContractByCreatedTime) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoMemContractByCreatedTime.Merge(dst, src)
+func (m *SoMemContractByCreatedTime) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoMemContractByCreatedTime.Merge(m, src)
 }
 func (m *SoMemContractByCreatedTime) XXX_Size() int {
 	return xxx_messageInfo_SoMemContractByCreatedTime.Size(m)
@@ -176,16 +179,17 @@ func (m *SoMemContractByAbi) Reset()         { *m = SoMemContractByAbi{} }
 func (m *SoMemContractByAbi) String() string { return proto.CompactTextString(m) }
 func (*SoMemContractByAbi) ProtoMessage()    {}
 func (*SoMemContractByAbi) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{3}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{3}
 }
+
 func (m *SoMemContractByAbi) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoMemContractByAbi.Unmarshal(m, b)
 }
 func (m *SoMemContractByAbi) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoMemContractByAbi.Marshal(b, m, deterministic)
 }
-func (dst *SoMemContractByAbi) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoMemContractByAbi.Merge(dst, src)
+func (m *SoMemContractByAbi) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoMemContractByAbi.Merge(m, src)
 }
 func (m *SoMemContractByAbi) XXX_Size() int {
 	return xxx_messageInfo_SoMemContractByAbi.Size(m)
@@ -214,16 +218,17 @@ func (m *SoMemContractByBalance) Reset()         { *m = SoMemContractByBalance{}
 func (m *SoMemContractByBalance) String() string { return proto.CompactTextString(m) }
 func (*SoMemContractByBalance) ProtoMessage()    {}
 func (*SoMemContractByBalance) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{4}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{4}
 }
+
 func (m *SoMemContractByBalance) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoMemContractByBalance.Unmarshal(m, b)
 }
 func (m *SoMemContractByBalance) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoMemContractByBalance.Marshal(b, m, deterministic)
 }
-func (dst *SoMemContractByBalance) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoMemContractByBalance.Merge(dst, src)
+func (m *SoMemContractByBalance) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoMemContractByBalance.Merge(m, src)
 }
 func (m *SoMemContractByBalance) XXX_Size() int {
 	return xxx_messageInfo_SoMemContractByBalance.Size(m)
@@ -252,16 +257,17 @@ func (m *SoMemContractByCode) Reset()         { *m = SoMemContractByCode{} }
 func (m *SoMemContractByCode) String() string { return proto.CompactTextString(m) }
 func (*SoMemContractByCode) ProtoMessage()    {}
 func (*SoMemContractByCode) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{5}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{5}
 }
+
 func (m *SoMemContractByCode) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoMemContractByCode.Unmarshal(m, b)
 }
 func (m *SoMemContractByCode) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoMemContractByCode.Marshal(b, m, deterministic)
 }
-func (dst *SoMemContractByCode) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoMemContractByCode.Merge(dst, src)
+func (m *SoMemContractByCode) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoMemContractByCode.Merge(m, src)
 }
 func (m *SoMemContractByCode) XXX_Size() int {
 	return xxx_messageInfo_SoMemContractByCode.Size(m)
@@ -291,16 +297,17 @@ func (m *SoListContractByCreatedTime) Reset()         { *m = SoListContractByCre
 func (m *SoListContractByCreatedTime) String() string { return proto.CompactTextString(m) }
 func (*SoListContractByCreatedTime) ProtoMessage()    {}
 func (*SoListContractByCreatedTime) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{6}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{6}
 }
+
 func (m *SoListContractByCreatedTime) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoListContractByCreatedTime.Unmarshal(m, b)
 }
 func (m *SoListContractByCreatedTime) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoListContractByCreatedTime.Marshal(b, m, deterministic)
 }
-func (dst *SoListContractByCreatedTime) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoListContractByCreatedTime.Merge(dst, src)
+func (m *SoListContractByCreatedTime) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoListContractByCreatedTime.Merge(m, src)
 }
 func (m *SoListContractByCreatedTime) XXX_Size() int {
 	return xxx_messageInfo_SoListContractByCreatedTime.Size(m)
@@ -336,16 +343,17 @@ func (m *SoUniqueContractById) Reset()         { *m = SoUniqueContractById{} }
 func (m *SoUniqueContractById) String() string { return proto.CompactTextString(m) }
 func (*SoUniqueContractById) ProtoMessage()    {}
 func (*SoUniqueContractById) Descriptor() ([]byte, []int) {
-	return fileDescriptor_so_contract_ce86407f55af5787, []int{7}
+	return fileDescriptor_6ccfb8b33a116ac5, []int{7}
 }
+
 func (m *SoUniqueContractById) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SoUniqueContractById.Unmarshal(m, b)
 }
 func (m *SoUniqueContractById) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_SoUniqueContractById.Marshal(b, m, deterministic)
 }
-func (dst *SoUniqueContractById) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_SoUniqueContractById.Merge(dst, src)
+func (m *SoUniqueContractById) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SoUniqueContractById.Merge(m, src)
 }
 func (m *SoUniqueContractById) XXX_Size() int {
 	return xxx_messageInfo_SoUniqueContractById.Size(m)
@@ -374,11 +382,9 @@ func init() {
 	proto.RegisterType((*SoUniqueContractById)(nil), "table.so_unique_contract_by_id")
 }
 
-func init() {
-	proto.RegisterFile("app/table/so_contract.proto", fileDescriptor_so_contract_ce86407f55af5787)
-}
+func init() { proto.RegisterFile("app/table/so_contract.proto", fileDescriptor_6ccfb8b33a116ac5) }
 
-var fileDescriptor_so_contract_ce86407f55af5787 = []byte{
+var fileDescriptor_6ccfb8b33a116ac5 = []byte{
 	// 355 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x93, 0xcf, 0x6a, 0xe3, 0x30,
 	0x10, 0xc6, 0x91, 0x93, 0xec, 0xb2, 0x93, 0xc0, 0x2e, 0x66, 0x9b, 0xba, 0xe9, 0xa1, 0xc6, 0x87,
@@ -386,9 +392,9 @@ var fileDescriptor_so_contract_ce86407f55af5787 = []byte{
 	0x19, 0xb0, 0x3d, 0x6e, 0x24, 0x1f, 0xf2, 0x06, 0x7d, 0xb0, 0x3e, 0x58, 0xb1, 0x93, 0x38, 0x2e,
 	0x35, 0xfd, 0x07, 0xbd, 0x18, 0x59, 0xf3, 0xcd, 0x37, 0xfc, 0xbe, 0x41, 0x70, 0x2a, 0x8a, 0x22,
 	0xb2, 0x22, 0x49, 0x75, 0x64, 0x88, 0x4b, 0xca, 0xed, 0x5a, 0x48, 0x1b, 0x16, 0x6b, 0xb2, 0xe4,
-	0x0e, 0xea, 0xc2, 0xe4, 0x7f, 0xfd, 0x67, 0x37, 0x85, 0x8e, 0xaa, 0xcf, 0xb6, 0x38, 0xf1, 0x0e,
-	0xb7, 0x59, 0x99, 0x5a, 0xe4, 0xa8, 0xb6, 0x95, 0xe0, 0x99, 0xc1, 0xb0, 0x65, 0xe6, 0x9e, 0x83,
-	0x83, 0xca, 0x63, 0x3e, 0x9b, 0x0e, 0xaf, 0xc6, 0x61, 0xd3, 0x16, 0xee, 0x05, 0x1c, 0x55, 0xec,
+	0x0e, 0xea, 0xc2, 0xc4, 0xab, 0xff, 0xec, 0xa6, 0xd0, 0x51, 0x56, 0xa6, 0x16, 0x39, 0xaa, 0xad,
+	0x60, 0xf2, 0xff, 0x50, 0xa9, 0x3e, 0xdb, 0xdb, 0xe0, 0x99, 0xc1, 0xb0, 0x65, 0xe6, 0x9e, 0x83,
+	0x83, 0xca, 0x63, 0x3e, 0x9b, 0x0e, 0xaf, 0xc6, 0x61, 0xd3, 0x12, 0xee, 0x05, 0x1c, 0x55, 0xec,
 	0xa0, 0x72, 0x6f, 0x60, 0x24, 0xd7, 0x5a, 0x58, 0xad, 0xb8, 0xc5, 0x4c, 0x7b, 0x4e, 0xdd, 0x71,
 	0xd2, 0xea, 0xa8, 0xae, 0x79, 0x41, 0x98, 0x5b, 0x6e, 0xb4, 0x8c, 0x87, 0x3b, 0xf9, 0x3d, 0x66,
 	0xda, 0xfd, 0x07, 0x3d, 0x91, 0xa0, 0xd7, 0xf3, 0xd9, 0xf4, 0x4f, 0x5c, 0x1d, 0xdd, 0x0b, 0xf8,
@@ -401,6 +407,6 @@ var fileDescriptor_so_contract_ce86407f55af5787 = []byte{
 	0xc6, 0xfe, 0x50, 0x0c, 0xbb, 0x7d, 0x38, 0x1f, 0xee, 0x63, 0x01, 0x9e, 0x21, 0x5e, 0xe6, 0xf8,
 	0x58, 0xea, 0x6f, 0xee, 0x74, 0x71, 0xf9, 0x30, 0x5b, 0xa2, 0x5d, 0x95, 0x49, 0x28, 0x29, 0x8b,
 	0x24, 0x19, 0xb9, 0x12, 0x98, 0x47, 0x95, 0x4c, 0xe7, 0x96, 0xcc, 0x7c, 0x49, 0x51, 0xf3, 0xa4,
-	0x92, 0x5f, 0xb5, 0xd1, 0xf5, 0x4b, 0x00, 0x00, 0x00, 0xff, 0xff, 0xcc, 0x5f, 0xd8, 0xc9, 0x66,
+	0x92, 0x5f, 0xb5, 0xd1, 0xf5, 0x4b, 0x00, 0x00, 0x00, 0xff, 0xff, 0xbf, 0x58, 0xd7, 0xd1, 0x66,
 	0x03, 0x00, 0x00,
 }
