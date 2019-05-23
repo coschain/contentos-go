9158b8df8533c42cfbaa07c51b079cc3d49d92a6
daishuo <daishuo@gmail.com>
2019-05-23 15:36:27 +0800

    trxs_to_mysql: replace big operation-type-switch-case blocks

--- tests/plugins/trx_to_mysql_server_test.go
+++ tests/plugins/trx_to_mysql_server_test.go
@@ -15,36 +15,7 @@ type Op map[string]interface{}
 func ReplaceOperation(operations []*prototype.Operation) []Op {
 	var ops []Op
 	for _, operation := range operations {
-		switch x := operation.Op.(type) {
-		case *prototype.Operation_Op1:
-			ops = append(ops, Op{"create_account":x.Op1})
-		case *prototype.Operation_Op2:
-			ops = append(ops, Op{"transfer": x.Op2})
-		case *prototype.Operation_Op3:
-			ops = append(ops, Op{"bp_register": x.Op3})
-		case *prototype.Operation_Op4:
-			ops = append(ops, Op{"bp_unregister": x.Op4})
-		case *prototype.Operation_Op5:
-			ops = append(ops, Op{"bp_vote": x.Op5})
-		case *prototype.Operation_Op6:
-			ops = append(ops, Op{"post": x.Op6})
-		case *prototype.Operation_Op7:
-			ops = append(ops, Op{"reply": x.Op7})
-		case *prototype.Operation_Op8:
-			ops = append(ops, Op{"follow": x.Op8})
-		case *prototype.Operation_Op9:
-			ops = append(ops, Op{"vote": x.Op9})
-		case *prototype.Operation_Op10:
-			ops = append(ops, Op{"transfer_to_vesting": x.Op10})
-		case *prototype.Operation_Op13:
-			ops = append(ops, Op{"contract_deploy": x.Op13})
-		case *prototype.Operation_Op14:
-			ops = append(ops, Op{"contract_apply": x.Op14})
-		case *prototype.Operation_Op15:
-			ops = append(ops, Op{"report": x.Op15})
-		case *prototype.Operation_Op16:
-			ops = append(ops, Op{"convert_vesting": x.Op16})
-		}
+		ops = append(ops, Op{prototype.GetGenericOperationName(operation): prototype.GetBaseOperation(operation)})
 	}
 	return ops
 }
