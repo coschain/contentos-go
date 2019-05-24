88d8f74d2e33d4974efe841a1e1d49cf3c3d0844
daishuo <daishuo@gmail.com>
2019-05-24 11:39:33 +0800

    remove unused protobuf type: contractData

--- prototype/contract_id.go
+++ prototype/contract_id.go
@@ -5,7 +5,3 @@ import "github.com/coschain/contentos-go/common/encoding/kope"
 func (m *ContractId) OpeEncode() ([]byte, error) {
 	return kope.Encode(m.Owner, m.Cname)
 }
-
-func (m *ContractDataId) OpeEncode() ([]byte, error) {
-	return kope.Encode(m.Owner, m.Cname, m.Pos)
-}
