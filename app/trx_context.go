coschain/contentos-go

commit 04c27811d6d34dc5f4813c29a47eef77e938a391

unit test

app/trx_context.go
@@ -120,7 +120,7 @@ func (p *TrxContext) RecordGasFee(caller string, spent uint64) {
 		newSpent := v.raw + spent
 		p.gasMap[caller].raw = newSpent
 	} else {
-		p.netMap[caller] = &resourceUnit{}
+		p.gasMap[caller] = &resourceUnit{}
 		p.gasMap[caller].raw = spent
 	}
 }
@@ -156,11 +156,15 @@ func NewTrxContext(wrapper *prototype.TransactionWrapper, db iservices.IDatabase
 	}
 }
 
-func NewTrxContextWithSigningKey(wrapper *prototype.TransactionWrapper, db iservices.IDatabaseRW, key *prototype.PublicKeyType) *TrxContext {
+func NewTrxContextWithSigningKey(wrapper *prototype.TransactionWrapper, db iservices.IDatabaseRW, key *prototype.PublicKeyType,control *TrxPool) *TrxContext {
 	return &TrxContext{
 		DynamicGlobalPropsRW: DynamicGlobalPropsRW{ db:db },
 		Wrapper: wrapper,
 		recoverPubs: []*prototype.PublicKeyType{ key },
+		gasMap: make(map[string]*resourceUnit),
+		netMap: make(map[string]*resourceUnit),
+		resourceLimiter: control.resourceLimiter,
+		control:control,
 	}
 }
 
@@ -296,7 +300,7 @@ func (p *TrxContext) ContractCall(caller, fromOwner, fromContract, fromMethod, t
 		Params: params,
 		Amount: &prototype.Coin{ Value: coins },
 	}
-	eval := &InternalContractApplyEvaluator{ctx: &ApplyContext{db: p.db, trxCtx: p, control: p.control}, op: op, remainGas: remainGas}
+	eval := &InternalContractApplyEvaluator{ctx: &ApplyContext{db: p.db, vmInjector: p, control: p.control}, op: op, remainGas: remainGas}
 	eval.Apply()
 }
 

app/evaluator.go
@@ -134,7 +134,7 @@ type UnStakeEvaluator struct {
 
 func (ev *AccountCreateEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Creator.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Creator.Value, constants.CommonOpGas)
 	creatorWrap := table.NewSoAccountWrap(ev.ctx.db, op.Creator)
 
 	opAssert(creatorWrap.CheckExist(), "creator not exist ")
@@ -182,7 +182,7 @@ func (ev *AccountCreateEvaluator) Apply() {
 
 func (ev *TransferEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.From.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.From.Value, constants.CommonOpGas)
 
 	// @ active_challenged
 	fromWrap := table.NewSoAccountWrap(ev.ctx.db, op.From)
@@ -204,7 +204,7 @@ func (ev *TransferEvaluator) Apply() {
 
 func (ev *PostEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
 
 	idWrap := table.NewSoPostWrap(ev.ctx.db, &op.Uuid)
 	opAssert(!idWrap.CheckExist(), "post uuid exist")
@@ -250,7 +250,7 @@ func (ev *PostEvaluator) Apply() {
 
 func (ev *ReplyEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
 
 	cidWrap := table.NewSoPostWrap(ev.ctx.db, &op.Uuid)
 	pidWrap := table.NewSoPostWrap(ev.ctx.db, &op.ParentUuid)
@@ -305,7 +305,7 @@ func (ev *ReplyEvaluator) Apply() {
 // no downvote has been supplied by command, so I ignore it
 func (ev *VoteEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Voter.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Voter.Value, constants.CommonOpGas)
 
 	voterWrap := table.NewSoAccountWrap(ev.ctx.db, op.Voter)
 	elapsedSeconds := ev.ctx.control.HeadBlockTime().UtcSeconds - voterWrap.GetLastVoteTime().UtcSeconds
@@ -378,7 +378,7 @@ func (ev *BpRegisterEvaluator) BpInWhiteList(bpName string) bool {
 
 func (ev *BpRegisterEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
 
 	opAssert(ev.BpInWhiteList(op.Owner.Value), "bp name not in white list")
 
@@ -404,7 +404,7 @@ func (ev *BpUnregisterEvaluator) Apply() {
 
 func (ev *BpVoteEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Voter.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Voter.Value, constants.CommonOpGas)
 
 	voterAccount := table.NewSoAccountWrap(ev.ctx.db, op.Voter)
 	voteCnt := voterAccount.GetBpVoteCount()
@@ -442,7 +442,7 @@ func (ev *BpVoteEvaluator) Apply() {
 
 func (ev *FollowEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Account.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Account.Value, constants.CommonOpGas)
 
 	acctWrap := table.NewSoAccountWrap(ev.ctx.db, op.Account)
 	opAssert(acctWrap.CheckExist(), "follow account do not exist ")
@@ -453,7 +453,7 @@ func (ev *FollowEvaluator) Apply() {
 
 func (ev *TransferToVestingEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.From.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.From.Value, constants.CommonOpGas)
 
 	fidWrap := table.NewSoAccountWrap(ev.ctx.db, op.From)
 	tidWrap := table.NewSoAccountWrap(ev.ctx.db, op.To)
@@ -475,7 +475,7 @@ func (ev *TransferToVestingEvaluator) Apply() {
 
 func (ev *ConvertVestingEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.From.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.From.Value, constants.CommonOpGas)
 
 	accWrap := table.NewSoAccountWrap(ev.ctx.db, op.From)
 	opAssert(accWrap.CheckExist(), "account do not exist")
@@ -583,7 +583,7 @@ func mergeTags(existed []int32, new []prototype.ReportOperationTag) []int32 {
 
 func (ev *ReportEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Reporter.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Reporter.Value, constants.CommonOpGas)
 	post := table.NewSoPostWrap(ev.ctx.db, &op.Reported)
 	opAssert(post.CheckExist(), "the reported post doesn't exist")
 	report := table.NewSoReportListWrap(ev.ctx.db, &op.Reported)
@@ -654,7 +654,7 @@ func (ev *ReportEvaluator) Apply() {
 
 func (ev *ContractDeployEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Owner.Value, constants.CommonOpGas)
 
 	cid := prototype.ContractId{Owner: op.Owner, Cname: op.Contract}
 	scid := table.NewSoContractWrap(ev.ctx.db, &cid)
@@ -725,7 +725,7 @@ func (ev *ContractApplyEvaluator) Apply() {
 
 	vmCtx := vmcontext.NewContextFromApplyOp(op, paramsData, code, abiInterface, tables, ev.ctx.vmInjector)
 	// set max gas
-	remain := ev.ctx.trxCtx.GetVmRemainCpuStamina(op.Caller.Value)
+	remain := ev.ctx.vmInjector.GetVmRemainCpuStamina(op.Caller.Value)
 	remainGas := remain * constants.CpuConsumePointDen
 	if remainGas > constants.MaxGasPerCall {
 		vmCtx.Gas = constants.MaxGasPerCall
@@ -822,7 +822,7 @@ func (ev *InternalContractApplyEvaluator) Apply() {
 
 func (ev *StakeEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Account.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Account.Value, constants.CommonOpGas)
 	accountWrap := table.NewSoAccountWrap(ev.ctx.db, op.Account)
 
 	value := &prototype.Coin{Value: op.Amount}
@@ -843,7 +843,7 @@ func (ev *StakeEvaluator) Apply() {
 
 func (ev *UnStakeEvaluator) Apply() {
 	op := ev.op
-	ev.ctx.trxCtx.RecordGasFee(op.Account.Value, constants.CommonOpGas)
+	ev.ctx.vmInjector.RecordGasFee(op.Account.Value, constants.CommonOpGas)
 
 	accountWrap := table.NewSoAccountWrap(ev.ctx.db, op.Account)
 

app/evaluator_base.go
@@ -29,7 +29,6 @@ type ApplyContext struct {
 	db         iservices.IDatabaseRW
 	control    iservices.IGlobalPropRW
 	vmInjector vminjector.Injector
-	trxCtx	   *TrxContext
 }
 
 type BaseEvaluator interface {

app/trx_pool.go
@@ -393,7 +393,7 @@ func (c *TrxPool) applyTransactionOnDb(db iservices.IDatabasePatch, entry *TrxEn
 	result := entry.GetTrxResult()
 	receipt, sigTrx := result.GetReceipt(), result.GetSigTrx()
 
-	trxContext := NewTrxContextWithSigningKey(result, db, entry.GetTrxSigningKey())
+	trxContext := NewTrxContextWithSigningKey(result, db, entry.GetTrxSigningKey(),c)
 //	if c.ctx.Config().ResourceCheck {
 		trxContext.CheckNet(uint64(proto.Size(sigTrx)))
 //	}
@@ -621,8 +621,8 @@ func (c *TrxPool) initGenesis() {
 	mustNoError(newAccountWrap.Create(func(tInfo *table.SoAccount) {
 		tInfo.Name = name
 		tInfo.CreatedTime = &prototype.TimePointSec{UtcSeconds: 0}
-		tInfo.Balance = prototype.NewCoin(constants.COSInitSupply - 1000)
-		tInfo.VestingShares = prototype.NewVest(1000)
+		tInfo.Balance = prototype.NewCoin(constants.COSInitSupply)
+		tInfo.VestingShares = prototype.NewVest(0)
 		tInfo.LastPostTime = &prototype.TimePointSec{UtcSeconds: 0}
 		tInfo.LastVoteTime = &prototype.TimePointSec{UtcSeconds: 0}
 		tInfo.NextPowerdownBlockNum = math.MaxUint32
@@ -630,6 +630,7 @@ func (c *TrxPool) initGenesis() {
 		tInfo.ToPowerdown = &prototype.Vest{Value: 0}
 		tInfo.HasPowerdown = &prototype.Vest{Value: 0}
 		tInfo.Owner = pubKey
+		tInfo.StakeVesting = prototype.NewVest(0)
 	}), "CreateAccount error")
 
 	// create account authority

app/trx_pool_test.go
@@ -165,7 +165,7 @@ func Test_PushTrx(t *testing.T) {
 
 	invoice := c.PushTrx(signedTrx)
 	if invoice.Status != prototype.StatusSuccess {
-		t.Error("PushTrx return status error:", invoice.Status)
+		t.Error("PushTrx return status error:", invoice.Status," info:",invoice.ErrorInfo)
 	}
 
 	bobName := &prototype.AccountName{Value: accountNameBob}

utils/resource_limit.go
@@ -77,6 +77,9 @@ func (s *ResourceLimiter) calculateUserMaxStamina(name string) uint64 {
 
 	totalVest := vest + stakeVest
 	allVest := dgpWrap.GetProps().TotalVestingShares.Value
+	if allVest == 0 {
+		return constants.OneDayStamina
+	}
 	userMax := float64( totalVest)/float64(allVest) * constants.OneDayStamina
 	return uint64(userMax)
 }

vm/injector/vm_injector.go
@@ -5,6 +5,7 @@ type Injector interface {
 	Log(msg string)
 	RequireAuth(name string) error
 	RecordGasFee(caller string, spent uint64)
+	GetVmRemainCpuStamina(name string) uint64
 	// only panic, no error return
 	TransferFromContractToUser(contract, owner, to string, amount uint64)
 	TransferFromUserToContract(from, contract, owner string, amount uint64)

commit 20dc88c052cc0369e663e771b8ee0c77b3ea6645

fix ApplyContext creation bug introduced by commit 04c27811

app/trx_context.go
@@ -289,6 +289,6 @@ func (p *TrxContext) ContractCall(caller, fromOwner, fromContract, fromMethod, t
 		Params: params,
 		Amount: &prototype.Coin{ Value: coins },
 	}
-	eval := &InternalContractApplyEvaluator{ctx: &ApplyContext{db: p.db, vmInjector: p, control: p.control, log:p.control.log}, op: op, remainGas: remainGas}
+	eval := &InternalContractApplyEvaluator{ctx: &ApplyContext{db: p.db, vmInjector: p, control: p, log:p.control.log}, op: op, remainGas: remainGas}
 	eval.Apply()
 }