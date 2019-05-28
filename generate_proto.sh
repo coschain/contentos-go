048b46d2c6cbbddab5da3491001174cca404bf39
daishuo <daishuo@gmail.com>
2019-05-28 15:05:29 +0800

    generate_proto.sh: removes dependency of GOPATH

--- generate_proto.sh
+++ generate_proto.sh
@@ -10,7 +10,6 @@ protoc --go_out=paths=source_relative:. prototype/*.proto
 cd ${CUR_DIR}/rpc/pb
 
 # RPC proto generate
-protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/coschain/contentos-go --go_out=plugins=grpc:. grpc.proto
-# protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/coschain/contentos-go --grpc-gateway_out=logtostderr=true:. grpc.proto
+protoc -I. -I../.. --go_out=plugins=grpc:. grpc.proto
 
 mockgen -source=grpc.pb.go > ../mock_grpcpb/mock_grpcpb.go
