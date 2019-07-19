c9d41a6e76c26effbfec9f2467ec35abf86a2aed
hanyunlong <chrisarthur@163.com>
2019-07-19 18:00:18 +0800

    health check can open and close port listen

--- AWSHealthCheck/healthcheck.go
+++ AWSHealthCheck/healthcheck.go
@@ -1,13 +1,19 @@
 package AWSHealthCheck
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/coschain/contentos-go/common"
 	"github.com/coschain/contentos-go/iservices"
 	"github.com/coschain/contentos-go/node"
 	"github.com/sirupsen/logrus"
 	"net/http"
-	"context"
 	"time"
+)
+
+const (
+	MaxDelayTime = 300  // second
 )
 
 var HealthCheckName = "healthcheck"
@@ -16,39 +22,103 @@ type AWSHealthCheck struct{
 	srv *http.Server
 	ctx *node.ServiceContext
 	log  *logrus.Logger
+	healthCheckON   bool
 }
 
 func NewAWSHealthCheck(ctx *node.ServiceContext, lg *logrus.Logger) (*AWSHealthCheck, error) {
-	s := &http.Server{Addr: fmt.Sprintf(":%s", ctx.Config().HealthCheck.Port)}
-	return &AWSHealthCheck{srv:s,log:lg, ctx:ctx}, nil
+	return &AWSHealthCheck{log:lg, ctx:ctx, healthCheckON:false}, nil
 }
 
 func (this *AWSHealthCheck) Start(node *node.Node) error {
 	http.HandleFunc("/", this.myHandler)
+
+	this.startServer(this.ctx)
+
+	s, err := this.ctx.Service(iservices.ConsensusServerName)
+	if err != nil {
+		return err
+	}
+	ctrl := s.(iservices.IConsensus)
+
 	go func(){
 		for {
-			s, err := this.ctx.Service(iservices.ConsensusServerName)
-			if err != nil {
-				return
+			headBlock, err := getHeadBlock(ctrl)
+			if headBlock == nil || err != nil {
+				this.log.Error("HealthCheck can not fetch head block ", err)
+				time.Sleep(time.Second * 10)
+				continue
 			}
-			ctrl := s.(iservices.IConsensus)
-			if ctrl.CheckSyncFinished() {
-				if err := this.srv.ListenAndServe(); err != http.ErrServerClosed {
-					this.log.Fatalf("ListenAndServe(): %s", err)
+
+			lastCommitBlock, err := getLastCommitBlock(ctrl)
+			if lastCommitBlock == nil || err != nil {
+				this.log.Error("HealthCheck can not fetch last commit block ", err)
+				time.Sleep(time.Second * 10)
+				continue
+			}
+
+			now := uint64(time.Now().Unix())
+			timeBetweenHeadBlockAndNow := now - headBlock.Timestamp()
+			timeBetweenLIBAndHeadBlock := headBlock.Timestamp() - lastCommitBlock.Timestamp()
+
+			if ctrl.CheckSyncFinished() && timeBetweenHeadBlockAndNow <= MaxDelayTime && timeBetweenLIBAndHeadBlock <= MaxDelayTime {
+				if !this.healthCheckON {
+					this.startServer(this.ctx)
 				}
-				break
+			} else {
+				if this.healthCheckON {
+					this.stopServer()
+				}
 			}
-			time.Sleep(time.Second / 10)
+
+			time.Sleep(time.Second)
 		}
 	}()
 	return nil
 }
 
 func (this *AWSHealthCheck) Stop() error {
-	this.srv.Shutdown(context.TODO())
+	this.stopServer()
 	return nil
 }
 
 func (this *AWSHealthCheck) myHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "——hi aws ALB, I'm alive ——\n")
+}
+
+func (this *AWSHealthCheck) startServer(ctx *node.ServiceContext) {
+	this.srv = &http.Server{Addr: fmt.Sprintf(":%s", ctx.Config().HealthCheck.Port)}
+	go this.srv.ListenAndServe()
+	this.healthCheckON = true
+}
+
+func (this *AWSHealthCheck) stopServer() {
+	ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+	if err := this.srv.Shutdown(ctx); err != nil {
+		this.log.Error("HealthCheck server shutdown error ", err)
+	}
+	this.healthCheckON = false
+}
+
+func getHeadBlock(ctrl iservices.IConsensus) (common.ISignedBlock, error) {
+	headBlockId := ctrl.GetHeadBlockId()
+
+	if headBlockId.BlockNum() == 0 {
+		return nil , errors.New("Chain empty")
+	}
+
+	iSignedBlock, err := ctrl.FetchBlock(headBlockId)
+
+	return iSignedBlock, err
+}
+
+func getLastCommitBlock(ctrl iservices.IConsensus) (common.ISignedBlock, error) {
+	lastCommitBlockId := ctrl.GetLIB()
+
+	if lastCommitBlockId.BlockNum() == 0 {
+		return nil , errors.New("Consensus not start up")
+	}
+
+	iSignedBlock, err := ctrl.FetchBlock(lastCommitBlockId)
+
+	return iSignedBlock, err
 }
