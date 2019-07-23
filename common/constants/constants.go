cd6318159c18c14c6c5c32f5f5370ba717142f78
zhaoguojie2010 <zhaoguojie2010@163.com>
2019-07-23 14:30:33 +0800

    set BlockProdRepetition to 5

--- common/constants/constants.go
+++ common/constants/constants.go
@@ -13,7 +13,7 @@ const (
 	VestSymbol = "VEST"
 
 	BlockInterval       = 1  // 1000 ms for one block produce
-	BlockProdRepetition = 10 // each producer produces 10 blocks in a row
+	BlockProdRepetition = 5 // each producer produces 5 blocks in a row
 
 	NoticeOpPre        = "oppre"
 	NoticeOpPost       = "oppost"
