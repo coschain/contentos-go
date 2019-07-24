aefb271280e434b366433c73601950afe8e176e6
hanyunlong <chrisarthur@163.com>
2019-07-24 18:09:03 +0800

    detector can detect all nodes' version with one ip

--- cmd/nodedetector/main.go
+++ cmd/nodedetector/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/coschain/contentos-go/cmd/nodedetector/detector"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+
+	"github.com/coschain/contentos-go/cmd/nodedetector/detector"
 )
 
 const (
@@ -14,6 +18,8 @@ const (
 // seed nodes of the chain, if you have multi seed nodes, please seperate ip with ","
 var seednodes = flag.String("seed", "", "seed nodes list")
 
+var stopSig = make(chan os.Signal, 1)
+
 func main() {
 	flag.Parse()
 
@@ -22,10 +28,42 @@ func main() {
 		return
 	}
 
+	nodeManager := detector.Init()
+	initSignal()
 	seedNodesList := strings.Split(*seednodes, ",")
 
-	for i:=0;i<len(seedNodesList);i++ {
-		endPoint := fmt.Sprintf("%s:%s", seedNodesList[i], RpcPort)
-		detector.RequireNodeInfo(endPoint)
+	nodeManager.AddToQuerylist(seedNodesList)
+
+	for {
+		select {
+		case <-stopSig:
+			detector.Wg.Wait()
+			fmt.Println("exit safe")
+			os.Exit(0)
+		case ip := <-detector.QueryList:
+			endPoint := fmt.Sprintf("%s:%s", ip, RpcPort)
+			detector.Wg.Add(1)
+			go nodeManager.Query(endPoint)
+		}
 	}
+}
+
+func initSignal() {
+	go func () {
+		SIGSTOP := syscall.Signal(0x13) //for windows compile
+		sigc := make(chan os.Signal, 1)
+		signal.Notify(sigc, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, SIGSTOP, syscall.SIGUSR1, syscall.SIGUSR2)
+		for {
+			s := <-sigc
+			fmt.Printf("get a signal %s\n", s.String())
+			switch s {
+			case syscall.SIGQUIT, syscall.SIGTERM, SIGSTOP, syscall.SIGINT:
+				stopSig <- s
+				return
+			default:
+				stopSig <- s
+				return
+			}
+		}
+	}()
 }
