53b1ddee23fc991366e5703a727c2a23a2402692
zhaoguojie2010 <zhaoguojie2010@163.com>
2019-07-29 16:45:10 +0800

    multi nodes: choose more validators

--- cmd/multinodetester/commands/test/shuffle.go
+++ cmd/multinodetester/commands/test/shuffle.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"github.com/coschain/contentos-go/iservices"
-	"math"
 	"math/rand"
 	"time"
 )
@@ -21,7 +20,8 @@ func (m *Monitor) Shuffle(names, sks []string, css iservices.IConsensus, stopCh 
 
 func (m *Monitor) shuffle(names, sks []string, css iservices.IConsensus) {
 	size := len(names)
-	seed := rand.Uint64() % uint64(math.Pow(2, float64(size-1)))
+	seed := rand.Uint64()
+	seed |= rand.Uint64()
 	if seed == 0 {
 		seed = 1
 	}
@@ -32,6 +32,12 @@ func (m *Monitor) shuffle(names, sks []string, css iservices.IConsensus) {
 		val[k] = v
 	}
 	m.RUnlock()
+
+	if val[names[0]] == false {
+		if err := RegesiterBP(names[0], sks[0], css); err != nil {
+			panic(err)
+		}
+	}
 
 	for i := 1; i < size; i++ {
 		if (seed>>uint(i)&1) == 1 && val[names[i]] == false {
