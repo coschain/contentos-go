9ce0f0cc932f52237339d3b4cda86bec11506979
daishuo <daishuo@gmail.com>
2019-07-30 22:38:56 +0800

    add a unittest reproducing precision-loss of int64's after json decoding

--- vm/contract/abi/abi_test.go
+++ vm/contract/abi/abi_test.go
@@ -4,7 +4,10 @@ import (
 	"github.com/coschain/contentos-go/common/encoding/vme"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"math/rand"
+	"reflect"
 	"testing"
+	"time"
 )
 
 
@@ -100,4 +103,33 @@ func TestAbi2(t *testing.T) {
 	data, err = vme.DecodeToJson(data, abi.TypeByName("test_arg").Type(), true)
 	r.NoError(err)
 	r.Equal(jsonStr, string(data))
+}
+
+func TestUint64Json(t *testing.T) {
+	var (
+		data []byte
+		err error
+	)
+	a := assert.New(t)
+	uint64Type := reflect.TypeOf(uint64(0))
+	rand.Seed(time.Now().UnixNano())
+	// try a few random uint64s
+	for i := 0; i < 5; i++ {
+		// generate a random decimal with 19 significands,
+		// which is a uint64 and can't be represented by float64.
+		digits := ""
+		for j := 0; j < 19; j++ {
+			digits += string('0' + rand.Intn(9) + 1)
+		}
+		// json -> vme encoded
+		data, err = vme.EncodeFromJson([]byte(digits), uint64Type)
+		a.NoError(err)
+
+		// vme encoded -> json
+		data, err = vme.DecodeToJson(data, uint64Type, true)
+		a.NoError(err)
+
+		// must be precisely equal
+		a.Equal(digits, string(data))
+	}
 }
