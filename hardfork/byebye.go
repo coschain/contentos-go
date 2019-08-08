b53466634ce610e3b9a2ff34939d51c6c4bbb4c8
zhaoguojie2010 <zhaoguojie2010@163.com>
2019-08-08 15:15:47 +0800

    add some hardfork demo cases

--- hardfork/byebye.go
+++
@@ -1,7 +0,0 @@
-package hardfork
-
-import "fmt"
-
-func byebye_20(args ...interface{}) {
-	fmt.Println("byebye_20")
-}
---
+++ hardfork/callback.go
@@ -0,0 +1,7 @@
+package hardfork
+
+var (
+	Uint64MIN ActionName = "Uint64MIN"
+	NewOP ActionName = "new_op"
+	NewVMNativeFunc ActionName = "new_vm_native_func"
+)
