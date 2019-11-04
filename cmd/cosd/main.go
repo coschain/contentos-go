7c66fc4712f72b37233bb520b77efae7f49fea98
huoxin <nwpudonkey@gmail.com>
2019-11-04 15:58:19 +0800

    crash info to file

--- cmd/cosd/main.go
+++ cmd/cosd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/coschain/cobra"
 	"github.com/coschain/contentos-go/cmd/cosd/commands"
 	"os"
+	"syscall"
 )
 
 // cosd is the main entry point into the system if no special subcommand is pointed
@@ -13,6 +15,8 @@ var rootCmd = &cobra.Command{
 	Use:   "cosd",
 	Short: "Cosd is a fast blockchain designed for content",
 }
+
+var globalFile *os.File
 
 func addCommands() {
 	rootCmd.AddCommand(commands.InitCmd())
@@ -25,4 +29,23 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func init() {
+	err := InitPanicFile()
+	if err != nil {
+		panic(fmt.Sprintf("init panic file failed, error:%v",err))
+	}
+}
+
+func InitPanicFile() error {
+	file, err := os.OpenFile("crash.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	globalFile = file
+	if err != nil {
+		return err
+	}
+	if err = syscall.Dup2(int(globalFile.Fd()), int(os.Stderr.Fd())); err != nil {
+		return err
+	}
+	return nil
 }
