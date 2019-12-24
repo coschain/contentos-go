590c500aee299fc37a6a9adec59c9d180e69cd95
aprocysanae <axdiaoqi220@gmail.com>
2019-12-24 12:13:57 +0800

    remove

--- app/plugins/blocklog_processor_iotrx_ops.go
+++ app/plugins/blocklog_processor_iotrx_ops.go
@@ -180,7 +180,6 @@ func ProcessConvertVestOperation(baseOp prototype.BaseOperation, baseRecord inte
 	}
 	ioTrx := baseRecord.(iservices.IOTrxRecord)
 	ioTrx.From = op.GetFrom().GetValue()
-	ioTrx.Amount = op.GetAmount().GetValue()
 	return []interface{}{ioTrx}, nil
 }
 
