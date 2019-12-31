1e7ea66a033c6652167fd367af1bcbd4f3378b5a
hanyunlong <chrisarthur@163.com>
2019-12-31 15:55:48 +0800

    cosd add fast-sync command

---
+++ cmd/cosd/commands/fastSync.go
@@ -0,0 +1,83 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/coschain/cobra"
+	"github.com/coschain/contentos-go/cmd/databackup/commands"
+	"github.com/coschain/contentos-go/common"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+var fullNode bool
+var downFileName string
+
+var FastSyncCmd = func() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:       "fast-sync",
+		Short:     "fast sync mainnet data",
+		Example:   "bp enable [bpname]",
+		Run:       syncMainnetData,
+	}
+	cmd.Flags().StringVarP(&cfgName, "name", "n", "", "node name (default is cosd)")
+	cmd.Flags().BoolVarP(&fullNode, "fullNode", "f", false, "start a full node or not")
+	return cmd
+}
+
+func syncMainnetData(cmd *cobra.Command, args []string) {
+	// read config to get data directory
+	cfg := readConfig()
+
+	// download file from s3
+	err := downloadFromS3()
+	if err != nil {
+		common.Fatalf("download From S3 error %v", err)
+	}
+
+	if cfg.DataDir != "" {
+		dir, err := filepath.Abs(cfg.DataDir)
+		if err != nil {
+			common.Fatalf("DataDir in cfg cannot be converted to absolute path")
+		}
+		cfg.DataDir = dir
+	}
+	dest := filepath.Join(cfg.DataDir, cfg.Name)
+	cmdStr := fmt.Sprintf("tar -zxvf %s -C %s", downFileName, dest)
+	bashCmd := exec.Command("/bin/bash","-c", cmdStr)
+	if err := bashCmd.Run(); err != nil {
+		common.Fatalf("failed to decompress data file %v", err)
+	}
+}
+
+func downloadFromS3() error {
+	s, err := session.NewSession(&aws.Config{Region: aws.String(commands.S3_REGION)})
+	if err != nil {
+		return err
+	}
+
+	if fullNode {
+		downFileName = commands.FULL_NODE_ARC_FILENAME
+	} else {
+		downFileName = commands.NON_FULL_NODE_ARC_FILENAME
+	}
+
+	file, err := os.Create(downFileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	downloader := s3manager.NewDownloader(s)
+	_, err = downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket:   aws.String(commands.S3_BUCKET),
+			Key:      aws.String(downFileName),
+		})
+	return err
+}
