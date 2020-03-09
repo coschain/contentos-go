ee22f2a8b2b4a705accdbeb9bdd06f9ce56aa183
daishuo <daishuo@gmail.com>
2020-03-09 18:53:53 +0800

    bug fixes and sql tests

--- app/plugins/blocklog_processor_vest_delegation.go
+++ app/plugins/blocklog_processor_vest_delegation.go
@@ -1,6 +1,8 @@
 package plugins
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/coschain/contentos-go/app/blocklog"
 	"github.com/coschain/contentos-go/app/table"
@@ -16,19 +18,19 @@ const (
 )
 
 type VestDelegationRecord struct {
-	ID uint64					`gorm:"primary_key;auto_increment"`
-	OrderID uint64				`gorm:"not null;unique_index"`
-	From string					`gorm:"index"`
-	To string					`gorm:"index"`
-	Amount uint64				`gorm:"index"`
-	CreatedAtBlock uint64		`gorm:"not null;index"`
-	CreatedAtTime time.Time
-	MaturedAtBlock uint64		`gorm:"not null;index"`
-	ClaimedAtBlock uint64		`gorm:"index"`
-	ClaimedAtTime time.Time
-	DeliveredAtBlock uint64		`gorm:"index"`
+	ID              uint64		`gorm:"primary_key;auto_increment"`
+	OrderID         uint64		`gorm:"not null;unique_index"`
+	From            string		`gorm:"index"`
+	To              string		`gorm:"index"`
+	Amount          uint64		`gorm:"index"`
+	CreatedAtBlock  uint64		`gorm:"not null;index"`
+	CreatedAtTime   time.Time
+	MaturityBlock   uint64 		`gorm:"not null;index"`
+	ClaimedAtBlock  uint64 		`gorm:"index"`
+	ClaimedAtTime   time.Time
+	DeliveryBlock   uint64 		`gorm:"index"`
 	DeliveredAtTime time.Time
-	Status string				`gorm:"index"`
+	Status          string		`gorm:"index"`
 }
 
 
@@ -57,35 +59,43 @@ func (p *VestDelegationProcessor) ProcessChange(db *gorm.DB, change *blocklog.St
 	if change.What != "VestDelegation" {
 		return nil
 	}
+	var rec *table.SoVestDelegation
+	var err error
 	if change.Kind == blocklog.ChangeKindCreate && change.Cause == "delegate_vest" {
-		rec := change.Change.After.(*table.SoVestDelegation)
+		if rec, err = p.convertToVestDelegation(change.Change.After); err != nil {
+			return err
+		}
 		return db.Create(&VestDelegationRecord{
-			OrderID:          rec.GetId(),
-			From:             rec.GetFromAccount().GetValue(),
-			To:               rec.GetToAccount().GetValue(),
-			Amount:           rec.GetAmount().GetValue(),
-			CreatedAtBlock:   rec.GetCreatedBlock(),
-			CreatedAtTime:    time.Unix(int64(blockLog.BlockTime), 0),
-			MaturedAtBlock:   rec.GetMaturityBlock(),
-			ClaimedAtBlock:   0,
-			ClaimedAtTime:    time.Unix(int64(constants.GenesisTime), 0),
-			DeliveredAtBlock: 0,
-			DeliveredAtTime:  time.Unix(int64(constants.GenesisTime), 0),
-			Status:           VestDelegationCreated,
+			OrderID:         rec.GetId(),
+			From:            rec.GetFromAccount().GetValue(),
+			To:              rec.GetToAccount().GetValue(),
+			Amount:          rec.GetAmount().GetValue(),
+			CreatedAtBlock:  rec.GetCreatedBlock(),
+			CreatedAtTime:   time.Unix(int64(blockLog.BlockTime), 0),
+			MaturityBlock:   rec.GetMaturityBlock(),
+			ClaimedAtBlock:  0,
+			ClaimedAtTime:   time.Unix(int64(constants.GenesisTime), 0),
+			DeliveryBlock:   0,
+			DeliveredAtTime: time.Unix(int64(constants.GenesisTime), 0),
+			Status:          VestDelegationCreated,
 		}).Error
 	} else if change.Kind == blocklog.ChangeKindUpdate && change.Cause == "un_delegate_vest" {
-		rec := change.Change.After.(*table.SoVestDelegation)
+		if rec, err = p.convertToVestDelegation(change.Change.After); err != nil {
+			return err
+		}
 		order := new(VestDelegationRecord)
 		if db.Where(VestDelegationRecord{OrderID: rec.GetId()}).First(order).RecordNotFound() {
 			return fmt.Errorf("vest delegation order not found. id=%d", rec.GetId())
 		}
 		order.ClaimedAtBlock = blockLog.BlockNum
 		order.ClaimedAtTime = time.Unix(int64(blockLog.BlockTime), 0)
-		order.DeliveredAtBlock = rec.GetDeliveryBlock()
+		order.DeliveryBlock = rec.GetDeliveryBlock()
 		order.Status = VestDelegationDelivering
 		return db.Save(order).Error
 	} else if change.Kind == blocklog.ChangeKindDelete && change.Cause == "esys.deliver_vest" {
-		rec := change.Change.Before.(*table.SoVestDelegation)
+		if rec, err = p.convertToVestDelegation(change.Change.Before); err != nil {
+			return err
+		}
 		order := new(VestDelegationRecord)
 		if db.Where(VestDelegationRecord{OrderID: rec.GetId()}).First(order).RecordNotFound() {
 			return fmt.Errorf("vest delegation order not found. id=%d", rec.GetId())
@@ -105,6 +115,19 @@ func (p *VestDelegationProcessor) Finalize(db *gorm.DB, blockLog *blocklog.Block
 	return nil
 }
 
+func (p *VestDelegationProcessor) convertToVestDelegation(data interface{}) (*table.SoVestDelegation, error) {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	rec := new(table.SoVestDelegation)
+	d := json.NewDecoder(bytes.NewReader([]byte(jsonBytes)))
+	d.UseNumber()
+	if err = d.Decode(rec); err != nil {
+		return nil, err
+	}
+	return rec, nil
+}
 
 func init() {
 	RegisterSQLTableNamePattern("vest_delegation_records")
