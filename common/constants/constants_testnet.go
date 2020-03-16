b2d8ecbd9be014f6cbce0cf8316e914833dbdd47
hanyunlong <chrisarthur@163.com>
2020-03-16 17:46:48 +0800

    set testnet hardfork3 block number

--- common/constants/constants_testnet.go
+++ common/constants/constants_testnet.go
@@ -28,5 +28,5 @@ const (
 	Original uint64 = 0
 	HardFork1 uint64 = 1375000
 	HardFork2 uint64 = 9734951
-	HardFork3 uint64 = 99999999	// TODO: SET CORRECT BLOCK NUMBER
+	HardFork3 uint64 = 14845355
 )
